a4087cac3f8cfc2533691a5dfe3f10cf1a5ac469
ddddwee1 <cy960823@outlook.com>
2015-12-22 23:22:40 +0800

    萨满写入了圣堂刺客
    还差治疗波和先祖之力的texture

--- vscripts/abilities/shaman_ancient_power_lua.lua
+++ vscripts/abilities/shaman_ancient_power_lua.lua
@@ -3,41 +3,32 @@ function AncientPower(keys)
     local ability = keys.ability;
     local ability_level = ability:GetLevel();
     local ability1_name = "shaman_summon_totem";
-    local ability2_name = "healing_wave";
-    local ability3_name = "ligntening_shield";
+    local ability3_name = "shaman_lightening_shield";
     
     local ability1 = caster:GetAbilityByIndex(0);
-    local ability2 = caster:GetAbilityByIndex(1);
     local ability3 = caster:GetAbilityByIndex(2);
 
     local level1 = ability1:GetLevel();
-    local level2 = ability2:GetLevel();
     local level3 = ability3:GetLevel();
 
     local ability1_oldname = ability1:GetAbilityName();
-    local ability2_oldname = ability2:GetAbilityName():
     local ability3_oldname = ability3:GetAbilityName();
     
     local ability1_newname = ability1_name..ability_level;
-    local ability2_newname = ability2_name..ability_level;
     local ability3_newname = ability3_name..ability_level;
+	
+	print(ability3_newname);
     
-    caster:AddAbility();
+    caster:AddAbility(ability1_newname);
     caster:SwapAbilities(ability1_oldname, ability1_newname, false, true);
     local handle_new_ability = caster:FindAbilityByName(ability1_newname);
     handle_new_ability:SetLevel(level1);
     caster:RemoveAbility(ability1_oldname);
     
-    caster:AddAbility();
-    caster:SwapAbilities(ability2_oldname, ability2_newname, false, true);
-    local handle_new_ability = caster:FindAbilityByName(ability2_newname);
-    handle_new_ability:SetLevel(level2);
-    caster:RemoveAbility(ability2_oldname);
-    
-    caster:AddAbility();
+    caster:AddAbility(ability3_newname);
     caster:SwapAbilities(ability3_oldname, ability3_newname, false, true);
-    local handle_new_ability = caster:FindAbilityByName(ability3_newname);
-    handle_new_ability:SetLevel(level3);
+    local handle_new_ability2 = caster:FindAbilityByName(ability3_newname);
+    handle_new_ability2:SetLevel(level3);
     caster:RemoveAbility(ability3_oldname);
     
 end
