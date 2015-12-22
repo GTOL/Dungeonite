8d423c9ff4917af7c5d1cb77adc19475f26b3a97
ddddwee1 <cy960823@outlook.com>
2015-12-22 23:38:04 +0800

    萨满写入圣堂刺客
    大概是这个样子，起码萨满的五个技能是好的，虽然我还没有修复大招刷新技能

--- vscripts/abilities/shaman_ancient_power_lua.lua
+++ vscripts/abilities/shaman_ancient_power_lua.lua
@@ -3,24 +3,21 @@ function AncientPower(keys)
     local ability = keys.ability;
     local ability_level = ability:GetLevel();
     local ability1_name = "shaman_summon_totem";
-    local ability2_name = "shaman_healing_wave";
     local ability3_name = "shaman_lightening_shield";
     
     local ability1 = caster:GetAbilityByIndex(0);
-    local ability2 = caster:GetAbilityByIndex(1);
     local ability3 = caster:GetAbilityByIndex(2);
 
     local level1 = ability1:GetLevel();
-    local level2 = ability2:GetLevel();
     local level3 = ability3:GetLevel();
 
     local ability1_oldname = ability1:GetAbilityName();
-    local ability2_oldname = ability2:GetAbilityName();
     local ability3_oldname = ability3:GetAbilityName();
     
     local ability1_newname = ability1_name..ability_level;
-    local ability2_newname = ability2_name..ability_level;
     local ability3_newname = ability3_name..ability_level;
+	
+	print(ability3_newname);
     
     caster:AddAbility(ability1_newname);
     caster:SwapAbilities(ability1_oldname, ability1_newname, false, true);
@@ -28,18 +25,10 @@ function AncientPower(keys)
     handle_new_ability:SetLevel(level1);
     caster:RemoveAbility(ability1_oldname);
     
-    caster:AddAbility(ability2_newname);
-    caster:SwapAbilities(ability2_oldname, ability2_newname, false, true);
-    handle_new_ability = caster:FindAbilityByName(ability2_newname);
-    handle_new_ability:SetLevel(level2);
-    caster:RemoveAbility(ability2_oldname);
-    
     caster:AddAbility(ability3_newname);
     caster:SwapAbilities(ability3_oldname, ability3_newname, false, true);
-    handle_new_ability = caster:FindAbilityByName(ability3_newname);
-    handle_new_ability:SetLevel(level3);
+    local handle_new_ability2 = caster:FindAbilityByName(ability3_newname);
+    handle_new_ability2:SetLevel(level3);
     caster:RemoveAbility(ability3_oldname);
     
 end
-
-
---
+++ vscripts/abilities/shaman_healing_wave_lua.lua
@@ -0,0 +1,69 @@
+shaman_healing_wave = class({})
+hCaster = nil;
+hTarget = nil;
+
+function shaman_healing_wave:CastFilterResultTarget(hTarget)
+	local nResult = UnitFilter( hTarget, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, self:GetCaster():GetTeamNumber());
+	return nResult;
+end
+
+function shaman_healing_wave:GetHealingPower(nLevel)
+	return self:GetLevelSpecialValueFor("Healing_Power",nLevel);
+end
+
+function shaman_healing_wave:GetUltiLevel()
+	local res = hCaster:GetAbilityByIndex(4):GetLevel();
+	return res;
+end
+
+function shaman_healing_wave:OnSpellStart()
+	local ent = Entities:CreateByClassname("info_target");
+	hCaster = self:GetCaster();
+	hTarget = self:GetCursorTarget();
+	
+	if (hCaster==nil) or (hTarget==nil) or (hTarget:TriggerSpellAbsorb(this)) then
+		return
+	end
+	local nTargetFX = ParticleManager:CreateParticle( "particles/units/heroes/hero_dazzle/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, hCaster )
+	ParticleManager:SetParticleControlEnt( nTargetFX, 1, hTarget, PATTACH_ABSORIGIN_FOLLOW, nil, hCaster:GetOrigin(), false )
+	ParticleManager:ReleaseParticleIndex( nTargetFX )
+	local vHeal = hTarget:GetHealth();
+	vHeal = vHeal + self:GetHealingPower(self:GetLevel());
+	hTarget:SetHealth(vHeal);
+	
+	
+	--countinuing finding heroes and imply healing
+	for i=1,(3+self:GetUltiLevel())
+	do
+		require "timers"
+		Timers:CreateTimer(i,function()
+			org = hTarget:GetOrigin();
+			local units = FindUnitsInRadius(hTarget:GetTeamNumber(),org,nil,400.0,DOTA_UNIT_TARGET_TEAM_FRIENDLY,DOTA_UNIT_TARGET_HERO+DOTA_UNIT_TARGET_CREEP,DOTA_UNIT_TARGET_FLAG_NONE,FIND_ANY_ORDER,false);
+			local nTarget = nil;
+			for _,unit in pairs(units)
+			do	
+				--Find the unit with least health percentage
+				--nTarget work as a buffer variable
+				if(hTarget~=unit) then
+					if (nTarget==nil) then
+						nTarget = unit;
+					else
+						if unit:GetHealth()/unit:GetMaxHealth() < unit:GetHealth()/unit:GetMaxHealth() then
+							nTarget = unit;
+						end
+					end
+				end
+			end
+			if(not nTarget) then
+				return;
+			end
+			nTargetFX = ParticleManager:CreateParticle( "particles/units/heroes/hero_dazzle/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, hTarget )
+			ParticleManager:SetParticleControlEnt( nTargetFX, 1, nTarget, PATTACH_ABSORIGIN_FOLLOW, nil, hCaster:GetOrigin(), false )
+			ParticleManager:ReleaseParticleIndex( nTargetFX )
+			hTarget = nTarget;
+			vHeal = hTarget:GetHealth();
+			vHeal = vHeal + self:GetHealingPower(self:GetLevel());
+			hTarget:SetHealth(vHeal);
+		end);
+	end
+end
