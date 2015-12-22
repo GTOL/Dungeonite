d3047ec7ee07baaaf0bd3530c85a1aa17b019a31
GTOL <gt531593545@126.com>
2015-12-22 16:44:41 +0800

    修改萨满大招 分离治疗波kv文件。

--- vscripts/abilities/shaman_ancient_power_lua.lua
+++ vscripts/abilities/shaman_ancient_power_lua.lua
@@ -15,29 +15,31 @@ function AncientPower(keys)
     local level3 = ability3:GetLevel();
 
     local ability1_oldname = ability1:GetAbilityName();
-    local ability2_oldname = ability2:GetAbilityName():
+    local ability2_oldname = ability2:GetAbilityName();
     local ability3_oldname = ability3:GetAbilityName();
     
     local ability1_newname = ability1_name..ability_level;
     local ability2_newname = ability2_name..ability_level;
     local ability3_newname = ability3_name..ability_level;
     
-    caster:AddAbility();
+    caster:AddAbility(ability1_newname);
     caster:SwapAbilities(ability1_oldname, ability1_newname, false, true);
     local handle_new_ability = caster:FindAbilityByName(ability1_newname);
     handle_new_ability:SetLevel(level1);
     caster:RemoveAbility(ability1_oldname);
     
-    caster:AddAbility();
+    caster:AddAbility(ability2_newname);
     caster:SwapAbilities(ability2_oldname, ability2_newname, false, true);
-    local handle_new_ability = caster:FindAbilityByName(ability2_newname);
+    handle_new_ability = caster:FindAbilityByName(ability2_newname);
     handle_new_ability:SetLevel(level2);
     caster:RemoveAbility(ability2_oldname);
     
-    caster:AddAbility();
+    caster:AddAbility(ability3_newname);
     caster:SwapAbilities(ability3_oldname, ability3_newname, false, true);
-    local handle_new_ability = caster:FindAbilityByName(ability3_newname);
+    handle_new_ability = caster:FindAbilityByName(ability3_newname);
     handle_new_ability:SetLevel(level3);
     caster:RemoveAbility(ability3_oldname);
     
 end
+
+
