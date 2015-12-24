a2432ae69c34806bc66af3b97ae8357ee1235a53
GTOL <gt531593545@126.com>
2015-12-25 00:09:02 +0800

    修改 大鱼人洪流

--- vscripts/abilities/slardar_torrent_lua.lua
+++ vscripts/abilities/slardar_torrent_lua.lua
@@ -30,19 +30,24 @@ function  torrent_prepare( keys )
 	local target_types = ability:GetAbilityTargetType()
 	local target_flags = ability:GetAbilityTargetFlags()
 	local units = FindUnitsInRadius(caster:GetTeamNumber(), target_location, nil, radius, target_teams, target_types, target_flags, FIND_CLOSEST, false )
-
+--对每个英雄操作
 	for _,unit in pairs(units) do
 		local unit_location = unit:GetAbsOrigin()
-		local vecter_distance = target_location - unit_location
-		local distance = (vecter_distance):Length2D()
-		local direction = (vecter_distance):Normalized()
+		local vector_distance = target_location - unit_location
+		local distance = (vector_distance):Length2D()
+		local direction = (vector_distance):Normalized()	--向中心向量
+		local rotation = Vector(direction.y, - direction.x, direction.z)	--垂直向量
 
-		unit.pull_speed = distance * 1/remaining_duration * 1/30
-		print(unit:HasModifier(torrent_modifier))
+		if unit.pull_speed == nil then
+			unit.pull_speed = distance * 1/(remaining_duration) * 1/30
+		end
+
 		if not unit:HasModifier(torrent_modifier) then
 			ability:ApplyDataDrivenModifier(caster, unit, torrent_modifier, {duration = remaining_duration})
 		end
-		unit:SetAbsOrigin(unit_location + direction * unit.pull_speed) 
+		
+		unit:SetAbsOrigin(unit_location + direction * unit.pull_speed + 3 * rotation) 
+
 	end
 end
 
