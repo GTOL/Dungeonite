3b149e81cf8e3dc08848d818fb9ccf5f7eef90f0
iamrockrepublic <theoneinsingapore@hotmail.com>
2016-01-09 18:43:10 +0800

    简单的刷怪函数
    在地图中设置entity，在函数中创建怪物

--- vscripts/addon_game_mode.lua
+++ vscripts/addon_game_mode.lua
@@ -22,15 +22,29 @@ end
 
 function CAddonTemplateGameMode:InitGameMode()
 	print( "Template addon is loaded." )
+	GameRules:SetPreGameTime(5.0)
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
 end
 
 -- Evaluate the state of the game
+Shua = 0
 function CAddonTemplateGameMode:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		--print( "Template addon script is running." )
+		if Shua == 0 then
+			Shua = 1
+			ShuaGuai()
+		end
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
 	return 1
+end
+
+function ShuaGuai()
+	local ShuaGuai_Entity = Entities:FindByName(nil, "ShuaGuai_1")
+	local Guai = CreateUnitByName("npc_elite_centaur", ShuaGuai_Entity:GetOrigin(),false,nil,nil,DOTA_TEAM_BADGUYS)
+	Guai:SetMustReachEachGoalEntity(true)
+	Guai:SetInitialGoalEntity(ShuaGuai_Entity)
+	Guai:AddNewModifier(nil,nil,"modifier_phased",{duration=0.1})
 end
