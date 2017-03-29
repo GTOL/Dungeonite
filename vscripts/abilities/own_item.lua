3460dd16c8908b36632b0ad7905ccc9b719ce2bc
ddddwee1 <cy960823@outlook.com>
2017-03-29 13:49:19 +0800

    更改了更改物品所有权技能的函数
    RT

--- vscripts/abilities/own_item.lua
+++ vscripts/abilities/own_item.lua
@@ -3,10 +3,8 @@ own_item = class({})
 --function sell:GetAOERadius()
 --	return self:GetSpecialValueFor("radius")
 --end
-
-
-
-function own_item:OnSpellStart() 
+--把这个技能设置为隐藏被动
+function own_item:OnInventoryContentsChanged() 
 	local hCaster = self:GetCaster() 
 	for i = 0, 5 do
 		local item = hCaster:GetItemInSlot( i )
